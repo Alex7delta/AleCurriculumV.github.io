35c36588c743c26c03959eb3a27b67cafb1e7824
Alex7delta <126512526+Alex7delta@users.noreply.github.com>
2023-03-23 12:08:46 -0300

    initial commit

---
+++ index.js/functions.js
@@ -0,0 +1,18 @@
+document.getElementById("boton").onclick=function(){ 
+console.log("capturamos elemento click"); 
+    document.getElementById("demo").innerHTML="343 4340019" ; 
+
+} 
+document.getElementById("boton1").onclick=function(){ 
+    console.log("capturamos elemento click"); 
+    document.getElementById("demo1").innerHTML="Victor Amari" ; 
+
+} 
+document.getElementById("boton2").onclick=function(){ 
+    document.getElementById("demo2").innerHTML=" Santos Dominguez 720, Ciudad de Paraná, Entre Ríos" ; 
+
+} 
+document.getElementById("boton3").onclick=function(){ 
+    document.getElementById("demo3").innerHTML="10 de Junio de 1975 en Santa Fe, República Argentina" ; 
+
+} 
